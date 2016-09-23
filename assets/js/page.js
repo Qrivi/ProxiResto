d8aa45b3dfc5ff35c1f698349cb40dc4e4f0afe0
Kristof Dewilde <me@krivi.be>
2016-09-23 02:20:33 +0200

    Base design is done

--- assets/js/page.js
+++ assets/js/page.js
@@ -9,22 +9,22 @@ $( document ).ready( function(){
         keyboardControl : true
     } );
 
+    $('#hamburger').click(function(){
+        $(this).toggleClass('open');
+    });
+
     $( window ).resize( function(){
 
         if( $( window ).width() < 650 ){
-            console.log( 'slides: 1' );
             swiper.params.slidesPerView = 1;
             swiper.params.grabCursor = true;
         }else if( $( window ).width() < 1100 ){
-            console.log( 'slides: 3' );
             swiper.params.slidesPerView = 3;
             swiper.params.grabCursor = true;
         }else if( $( window ).width() < 1295 ){
-            console.log( 'slides: 4' );
             swiper.params.slidesPerView = 4;
             swiper.params.grabCursor = true;
         }else{
-            console.log( 'slides: 5' );
             swiper.params.slidesPerView = 5;
             swiper.params.grabCursor = false;
         }
