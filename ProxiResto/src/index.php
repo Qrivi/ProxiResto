a1b99e545b6a6de22496fefb39b5ae651cb9294d
Kristof Dewilde <me@krivi.be>
2016-10-05 02:09:37 +0200

    Wrote nice controller for /{academic_year}/{week}

--- ProxiResto/src/index.php
+++ ProxiResto/src/index.php
@@ -6,35 +6,63 @@
     require_once './config/config.php';
 
     spl_autoload_register( function ( $classname ){
-        require( "../classes/" . $classname . ".php" );
+        require( '../classes/' . $classname . '.php' );
     } );
 
-    $app = new \Slim\App( [ "settings" => $config ] );
+    $app = new \Slim\App( [ 'settings' => $config ] );
     $container = $app->getContainer();
-    $container['view'] = new \Slim\Views\PhpRenderer( "./templates/" );
-    $container['pdo'] = new \Slim\PDO\Database( $config["db"]["dbms"] . ":" .
-                                                "host:" . $config["db"]["dbms"] . ";" .
-                                                "dbname:" . $config["db"]["name"] . ";" .
-                                                "charset:" . $config["db"]["charset"],
-                                                $config["db"]["user"], $config["db"]["pass"] );
+    $container['view'] = new \Slim\Views\PhpRenderer( './templates/' );
+    $container['pdo'] = new \Slim\PDO\Database( $config['db']['dbms'] . ':' .
+                                                'host:' . $config['db']['dbms'] . ';' .
+                                                'dbname:' . $config['db']['name'] . ';' .
+                                                'charset:' . $config['db']['charset'],
+                                                $config['db']['user'], $config['db']['pass'] );
 
     /* routes */
 
-    $app->get( '/', function ( Request $request, Response $response ){
+    $app->get( '/test', function ( Request $request, Response $response ){
         $selectStatement = $this->pdo->select()
-                                     ->from( 'proxiresto.menu' );
+                                     ->from( 'kristhb42_resto.menu' );
 
         $stmt = $selectStatement->execute();
         $data = $stmt->fetchAll();
 
-        $response = $this->view->render( $response, "hello.phtml", ["data", $data] );
+        $response = $this->view->render( $response, 'hello.phtml', [ 'data', $data ] );
         return $response;
     } );
 
-    $app->get( '/hello/{name}', function ( Request $request, Response $response ){
-        $name = $request->getAttribute( 'name' );
-        $response->getBody()->write( "Hello, $name" );
+    $app->get( '/', function ( Request $request, Response $response ) use ( $app ){
+        $date = date( 'y' );
+        $lang = 'nl'; //TODO get from cookie
 
+        $week = date( 'W' );
+        $year = ( $week > 35 ? ( $date . '-' . ( $date + 1 ) ) : ( ( $date - 1 ) . '-' . $date ) );
+
+        //$app->redirect( '/nl/year/week', 301 );
+        return $response->withRedirect( $request->getUri()->getBaseUrl() . "/{$lang}/{$year}/{$week}" );
+    } );
+
+    $app->get( '/nl/{year}/{week}', function ( Request $request, Response $response ){
+        $week = $request->getAttribute( 'week' );
+        $year = $request->getAttribute( 'year' );
+        $menu = [];
+
+        $stmt = $this->pdo->select( [ 'day', 'type', 'name', 'name_en', 'price', 'price_ext', 'veggie' ] )
+                          ->from( 'kristhb42_resto.menu' )
+                          ->whereMany( [ 'week' => $week, 'year' => $year ], '=' )
+                          ->execute();
+        $data = $stmt->fetchAll();
+
+        foreach( $data as &$item )
+            $menu[$item['day']][$item['type']][] = $item;
+
+        $response = $this->view->render( $response, 'menu_nl.phtml', [
+            'request' => $request,
+            'week'    => $week,
+            'year'    => '20' . ( $week > 35 ? substr( $year, 0, 2 ) : substr( $year, -2, 2 ) ),
+            'menu'    => $menu
+        ] );
         return $response;
     } );
+
     $app->run();
